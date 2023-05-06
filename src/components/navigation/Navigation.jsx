515f4d3bd082994dac92c24d366e6b1f0cb3e57a
percyZ <percyzmr@gmail.com>
2023-05-06 11:48:17 -0500

    asdf

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Text, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from  '../../screens/LoginScreen.jsx';
+import LoginScreen from  '../../screens/loginScreen';
 
 const Stack = createNativeStackNavigator();
 
